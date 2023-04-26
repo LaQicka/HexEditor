51e8ddd5ec583fa0c67240a738210461a2542b1a
LaQicka <laqicka1@gmail.com>
2023-04-26 15:45:08 +0300

    Add base north panel with menu. Already able to open files and change width

---
+++ src/gui/NorthPanel.java
@@ -0,0 +1,76 @@
+package gui;
+
+import logic.Application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+public class NorthPanel extends JMenuBar {
+    private String fileName;
+    private String path;
+    private Application app;
+
+    public NorthPanel(Application app){
+        this.app = app;
+
+        JComboBox width = new JComboBox();
+        width.setMaximumSize(new Dimension(50,50));
+        width.addItem(8);
+        width.addItem(16);
+        width.addItem(32);
+
+        JButton open = new JButton("Open");
+
+        JButton save = new JButton("Save");
+
+        JMenu file = new JMenu("File");
+        file.add(open);
+        file.add(save);
+
+// метод отслеживание выбора пользователем файла
+        open.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int result = fileChooser.showOpenDialog(file);
+
+                if(result == JFileChooser.APPROVE_OPTION){
+                    File selected = fileChooser.getSelectedFile();
+                    fileName = selected.getName();
+                    path = selected.getPath();
+                    try {
+                        app.readFile(path);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });
+
+// метод сохранение по нажатию нового файла
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+// метод отслеживание изменения пользователем количества колонок текста
+        width.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int selected = (int)width.getSelectedItem();
+                app.setWidth(selected);
+            }
+        });
+
+        this.add(file);
+        this.add(width);
+
+        this.setVisible(true);
+    }
+
+}
